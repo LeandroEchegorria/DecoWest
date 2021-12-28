46548f1a021d073ef596f6b0eab0946642836e27
Leandro Echegorria <leandro.echegorria@gmail.com>
2021-12-28 20:44:47 -0300

    Desafío: Incorporar ARRAYS

--- js/clases.js
+++ js/clases.js
@@ -1,4 +1,4 @@
-class Articulo {
+class Articulo { 
     constructor (nombre, precio, id) {
         this.nombre = nombre.toUpperCase();
         this.precio = parseFloat (precio);
@@ -13,3 +13,20 @@ class Articulo {
         console.log("Articulo: "+ this.nombre + ". Vendido:"+this.vendido);
     }
 };
+
+//declaracion de objetos de la clase Articulo
+const articulo1= new Articulo ("Cartuchera", 200, 001);
+const articulo2= new Articulo ("Cuaderno", 350, 002);
+const articulo3= new Articulo ("Set de Mate", 700, 003);
+const articulo4= new Articulo ("Sahumerio", 250 , 004);
+
+//declaracion de Array
+const arrayArticulos= [articulo1, articulo2, articulo3, articulo4];
+
+console.log("A continuación se muestran los artículos:");
+for (const articulo of arrayArticulos){  
+    console.log(articulo.nombre);
+    console.log(articulo.precio);
+};
+    
+
--- js/funciones.js
+++ js/funciones.js
@@ -4,13 +4,11 @@ function pedirDatos(){
             this.nombre= nombre.toUpperCase();
             this.edad= parseInt(edad);
         };
-        imprimir() {
-            console.log("El usuario se llama: "+this.nombre+", y tiene "+this.edad+" años");
-        }
+        imprimir() {console.log("El usuario se llama: "+this.nombre+", y tiene "+this.edad+" años");}
     };
     
     const usuario1= new Usuario (prompt("Ingresa tu nombre"), parseInt (prompt("¿Cuantos años tenes?")));
-        
+  
     nacido=2021-usuario1.edad;
     let msg="";
     if (usuario1.edad >= 21) {
@@ -21,13 +19,10 @@ function pedirDatos(){
     msg=("Edad ingresada no es correcta");
     alert("Naciste en el año " + nacido);
     alert(msg);
- 
     usuario1.imprimir();
     console.log("Gracias por participar", usuario1.nombre);
-
 };
-
-
+ 
 function comprar (){
     let articulo= parseInt(prompt ("¿Qué articulo desea comprar? 1-Cartuchera ($200); 2-Cuaderno ($350); 3-Set Mate ($700); 4-Sahumerios ($250)"));
     console.log("Articulo = ", articulo);   
