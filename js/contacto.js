91d4db2168e4dbb772dd26c11ff0cedf9ec7c647
Leandro Echegorria <leandro.echegorria@gmail.com>
2022-01-29 22:55:09 -0300

    Entrega Desafio - Animaciones concatenadas

--- js/contacto.js
+++ js/contacto.js
@@ -2,16 +2,16 @@
 function verContacto() {
 
     $("#formulario").prepend (`<h3>Formulario de contacto</h3>
-        <p><label>Nombre Completo:</label></p>
+        <p><label id="name">Nombre Completo:</label></p>
         <input type="text" name="nombre" id="name"/>
             
-        <p><label>Email:</label></p>
+        <p><label id="email">Email:</label></p>
         <input type="text" name="email" id="email"/>
 
-        <p><label>Telefono:</label></p>            
+        <p><label  id="phone">Telefono:</label></p>            
         <input type="text" name="telefono" id="phone"/>
             
-        <p><label>Comentario:</label></p>
+        <p><label id="comment">Comentario:</label></p>
         <p><textarea name="mensaje" id="comment"></textarea></p>
         <input type="submit" value="Enviar"/>`
     );
@@ -20,7 +20,6 @@ verContacto();
 
 
 $("#formulario").submit(function(e){
-    console.log(e);
     e.preventDefault();
     verificaForm(); 
 });
@@ -31,12 +30,26 @@ function verificaForm () {
     let phone= $("#phone")[0].value;
     
     if (name.length == 0) {
-        $("#formulario").append(`<h6>Campo "Nombre" obligatorio</h6>`);
+        $("#formulario").append(`<h6 class="error">Campo "Nombre" obligatorio</h6>`);
     } else if (email.length == 0) {
-            $("#formulario").append(`<h6>Campo "Email" obligatorio</h6>`);
+            $("#formulario").append(`<h6 class="error">Campo "Email" obligatorio</h6>`);
         } else if (phone.length == 0) {
-                $("#formulario").append(`<h6>Campo "Telefono" obligatorio</h6>`);
-            } else {$("#formulario").append(`<h4>Formulario enviado</h4>`);
+                $("#formulario").append(`<h6 class="error">Campo "Telefono" obligatorio</h6>`);
+            } else {$("#formulario").append(`<h4 class="error">Formulario enviado</h4>`);
             console.log("formulario enviado");
         };    
 };
+
+
+$('#contenedor').hide().slideUp(1500).slideDown(1500);
+$('#mapa').hide().delay(2000).slideDown(1500);
+$('#name').hide().delay(600).slideDown(1500);
+$('#email').hide().delay(1200).slideDown(1500);
+$('#phone').hide().delay(1800).slideDown(1500);
+$('#comment').hide().delay(2400).slideDown(1500);
+$('input').hide().delay(2400).slideDown(1500);
+$('textarea').hide().delay(2400).slideDown(1500);
+
+
+
+
